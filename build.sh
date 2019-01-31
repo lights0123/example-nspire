637f1cbe77119b4362d3ca64f95a7915e7af0260
lights0123 <developer@lights0123.com>
2019-01-30 23:13:56 -0500

    Fail build earlier

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
+set -e
 NAME=$(cargo read-manifest | jq -r '.name')
 TARGET=armv5te-nspire-eabi
-set -e
 cargo xbuild --target "$TARGET.json" 2>&1
 genzehn --input "target/$TARGET/debug/$NAME" --output "target/$TARGET/debug/$NAME.zehn" --name "hello_world"
 make-prg "target/$TARGET/debug/$NAME.zehn" hello_world.tns
