049841acd482ae9c8e8671bcd636c56190854519
lights0123 <developer@lights0123.com>
2019-02-01 10:29:47 -0500

    More debugging

--- hello-world-sdl/src/main.rs
+++ hello-world-sdl/src/main.rs
@@ -7,9 +7,29 @@ use core::cmp;
 
 use ndless_sdl::video::{SurfaceFlag, VideoFlag};
 use nspire::prelude::*;
-
+#[derive(PartialEq, Copy, Clone)]
+pub struct Rect {
+	pub x: u32,
+	pub y: u32,
+	pub w: u32,
+	pub h: u32
+}
 #[no_mangle]
 fn main() {
+	let a = Rect {
+		x: 0,
+		y: 0,
+		w: 240,
+		h: 240
+	};
+	nspire::msg::msg("msg", format!("{}", a.w));
+	let button_pressed = nspire::msg::msg_3b("Hello", "Hello, World!", "1", "2", "3");
+	let message = match button_pressed {
+		nspire::msg::Button::ONE => "one",
+		nspire::msg::Button::TWO => "two",
+		nspire::msg::Button::THREE => "three",
+	};
+	nspire::msg::msg("Title", format!("You pressed button number {}!", message));
 	ndless_sdl::init(&[ndless_sdl::InitFlag::Video]);
 	let screen = match ndless_sdl::video::set_video_mode(320, 240, 16,
 	                                                     &[SurfaceFlag::SWSurface],
@@ -17,21 +37,21 @@ fn main() {
 		Ok(screen) => screen,
 		Err(err) => panic!("failed to set video mode: {}", err)
 	};
-	// TODO: free font
 	let font = unsafe { ndless_sdl::nSDL_LoadFont(0, 255, 255, 255) };
-	let mut j = 0u64;
+	unsafe { ndless_sys::msleep(1000); }
+//	let mut j = 0u32;
 	'main: loop {
-		for i in 0usize..10 {
+		/*for i in 0usize..10 {
 			for j in 0usize..10 {
-				screen.fill_rect(Some(ndless_sdl::Rect {
-					x: (i as i16) * 800 / 10,
-					y: (j as i16) * 600 / 10,
-					w: 800 / 10,
-					h: 600 / 10
+				(Some(ndless_sdl::Rect {
+					x: 0,
+					y: 0,
+					w: 240,
+					h: 240
 				}), ndless_sdl::video::RGB(142, 120, 255));
 			}
-		}
-		let keys = nspire::input::get_keys();
+		}*/
+		/*let keys = nspire::input::get_keys();
 		if keys.contains(&nspire::input::Key::KEY_NSPIRE_ESC) { break; }
 		let mut message = format!("{:?}", keys);
 		let mut i = 0;
@@ -48,7 +68,12 @@ fn main() {
 		unsafe {
 			ndless_sdl::nSDL_DrawString(screen.raw, font, 0, 100, msg.as_ptr());
 		}
-		j += 1;
+		j += 1;*/
+//		j += 1;
+		/*let msg = nspire::cstr!(format!("{}", j));
+		unsafe {
+			ndless_sdl::nSDL_DrawString(screen.raw, font, 0, 100, msg.as_ptr());
+		}*/
 		screen.flip();
 	}
 	ndless_sdl::quit();
--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -4,9 +4,25 @@
 use nspire::prelude::*;
 extern crate ndless_handler;
 
+#[derive(Debug, PartialEq, Copy, Clone)]
+pub struct Rect {
+    pub x: u32,
+    pub y: u32,
+    pub w: u32,
+    pub h: u32
+}
 #[no_mangle]
 fn main() {
     use nspire::msg::*;
+    let a = Rect {
+        x: 0,
+        y: 0,
+        w: 240,
+        h: 240
+    };
+    msg("msg", "msg2");
+    msg("msg", format!("{:?}", a));
+    msg("msg", format!("{}", a.w));
     let button_pressed = msg_3b("Hello", "Hello, World!", "1", "2", "3");
     let message = match button_pressed {
         Button::ONE => "one",
