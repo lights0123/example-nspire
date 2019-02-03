090a0a605bd9e83cd823aa4f119223ce8b8efc48
lights0123 <developer@lights0123.com>
2019-02-02 22:56:44 -0500

    Add prompt demo

---
+++ hello-world-prompt/src/main.rs
@@ -0,0 +1,23 @@
+#![no_std]
+#![no_main]
+
+extern crate ndless_handler;
+
+use nspire::prelude::*;
+
+#[no_mangle]
+fn main() {
+	use nspire::msg::*;
+	match msg_numeric("title", "subtitle", "msg", 0, 5) {
+		None => msg("result", "You cancelled the operation!"),
+		Some(num) => msg("result", format!("You typed {}!", num)),
+	}
+	match msg_2numeric("title", "subtitle", "msg1", 0, 5, "msg2", 6, 10) {
+		None => msg("result", "You cancelled the operation!"),
+		Some((num1, num2)) => msg("result", format!("You typed {} and {}!", num1, num2)),
+	}
+	match msg_input("title", "subtitle", "msg") {
+		None => msg("result", "You cancelled the operation!"),
+		Some(response) => msg("result", format!("You typed \"{}\"!", response)),
+	}
+}
